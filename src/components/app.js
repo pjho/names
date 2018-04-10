4cc9dfc3de09d298e5105a34937cce95c4027c57
Pat Horsley <p@pho.sx>
2018-04-10 21:57:36 +1200

    eslint config

--- src/components/app.js
+++ src/components/app.js
@@ -1,53 +1,52 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { h, Component } from "preact";
+import { Router } from "preact-router";
 
-import Header from './header';
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Header from "./header";
+import Home from "../routes/home";
+import Profile from "../routes/profile";
 // import Home from 'async!../routes/home';
 // import Profile from 'async!../routes/profile';
 
 if (module.hot) {
-	require('preact/debug');
+  require("preact/debug");
 }
 
 export default class App extends Component {
-    state = {
-      loading: true,
-      data: {}
-    }
+  state = {
+    loading: true,
+    data: {}
+  };
 
-    /** Gets fired when the route changes.
-     *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-     *	@param {string} event.url	The newly routed URL
-     */
-    handleRoute = e => {
-      this.currentUrl = e.url;
-    };
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-  componentWillMount () {
+  componentWillMount() {
     fetch("assets/data/baby-names.json")
       .then(resp => resp.json())
       .then(json => {
         this.setState({ data: json, loading: false });
-      })
+      });
   }
 
-	render(props, { data, loading }) {
-
-		return (
-			<div id="app">
-				<Header />
+  render(props, { data, loading }) {
+    return (
+      <div id="app">
+        <Header />
         {loading ? (
           <p>Loading</p>
-        ): (
+        ) : (
           <Router onChange={this.handleRoute}>
-					<Home path="/" data={ data } thing='it' />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
+            <Home path="/" data={data} thing="it" />
+            <Profile path="/profile/" user="me" />
+            <Profile path="/profile/:user" />
+          </Router>
         )}
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
